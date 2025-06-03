da508cf0d3845f27474b5de7e19327b5b9708674
S_Stoykov26 <thelifeishere@abv.bg>
2025-06-04 00:36:59 +0300

    Add docker config

--- app.py
+++ app.py
@@ -88,4 +88,4 @@ def segment_nails():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
